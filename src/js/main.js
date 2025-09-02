acbb4ffa41a56d82c03f1c41868beeb00c00510b
LepkoQQ <lepko.san@gmail.com>
2025-09-02 17:36:41 +0200

    load and display xw grid and clues

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,134 @@
-const dropZone = document.querySelector(".drop-zone");
-const dropOutput = document.querySelector(".drop-output");
+const dropZoneEl = document.querySelector(".drop-zone");
+const puzzleContainerEl = document.querySelector(".puzzle-container");
 
-dropZone.addEventListener("dragover", (event) => {
+dropZoneEl.addEventListener("dragover", (event) => {
   event.preventDefault();
-  dropZone.classList.add("highlight");
+  dropZoneEl.classList.add("highlight");
 });
 
-dropZone.addEventListener("dragleave", () => {
-  dropZone.classList.remove("highlight");
+dropZoneEl.addEventListener("dragleave", () => {
+  dropZoneEl.classList.remove("highlight");
 });
 
-dropZone.addEventListener("drop", (event) => {
+dropZoneEl.addEventListener("drop", (event) => {
   event.preventDefault();
-  dropZone.classList.remove("highlight");
+  dropZoneEl.classList.remove("highlight");
 
   const files = event.dataTransfer.files;
   if (files.length > 0) {
     const reader = new FileReader();
     reader.onload = (event) => {
-      dropOutput.value = event.target.result;
+      loadNYTXW(JSON.parse(event.target.result));
     };
     reader.readAsText(files[0]);
-    dropZone.classList.add("hidden");
+    // dropZoneEl.classList.add("hidden");
   }
 });
+
+function formatDate(isoDate) {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return new Date(isoDate).toLocaleDateString("en-US", options);
+}
+
+function joinList(list) {
+  const options = { style: "long", type: "conjunction" };
+  return new Intl.ListFormat("en-US", options).format(list);
+}
+
+function loadMetadata(json) {
+  const metaEl = document.querySelector(".puzzle-meta");
+  const titleEl = metaEl.querySelector(".title");
+  const dateEl = metaEl.querySelector(".date");
+  const constructorsEl = metaEl.querySelector(".constructors");
+  const editorEl = metaEl.querySelector(".editor");
+
+  titleEl.textContent = json.title || "Untitled";
+  dateEl.textContent = formatDate(json.publicationDate);
+  constructorsEl.textContent = `Constructed by: ${joinList(json.constructors)}`;
+  editorEl.textContent = `Edited by: ${json.editor}`;
+}
+
+function buildClueLists(clueLists, clues) {
+  const clueListsEl = document.querySelector(".puzzle-clue-lists");
+  clueListsEl.innerHTML = "";
+
+  clueLists.forEach((clueList) => {
+    const listWrapperEl = document.createElement("div");
+    listWrapperEl.classList.add("clue-list");
+
+    const listNameEl = document.createElement("h3");
+    listNameEl.classList.add("name");
+    listNameEl.textContent = clueList.name;
+    listWrapperEl.appendChild(listNameEl);
+
+    const ulEl = document.createElement("ul");
+    clueList.clues.forEach((clueIdx) => {
+      const clue = clues[clueIdx];
+      const liEl = document.createElement("li");
+      liEl.dataset.clue = clueIdx;
+      if (clue.cells?.length) {
+        liEl.dataset.cells = JSON.stringify(clue.cells);
+      }
+      if (clue.relatives?.length) {
+        liEl.dataset.relatives = JSON.stringify(clue.relatives);
+      }
+      const labelEl = document.createElement("span");
+      labelEl.classList.add("label");
+      labelEl.textContent = `${clue.label}`;
+      liEl.appendChild(labelEl);
+      const textEl = document.createElement("span");
+      textEl.classList.add("text");
+      textEl.textContent = clue.text[0].plain;
+      liEl.appendChild(textEl);
+      ulEl.appendChild(liEl);
+    });
+    listWrapperEl.appendChild(ulEl);
+
+    clueListsEl.appendChild(listWrapperEl);
+  });
+}
+
+function buildGrid(dimensions, cells) {
+  const gridEl = document.querySelector(".puzzle-grid");
+  const { width, height } = dimensions;
+
+  gridEl.innerHTML = "";
+  gridEl.dataset.width = width;
+  gridEl.dataset.height = height;
+  gridEl.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+  gridEl.style.fontSize = `${15 / width}rem`;
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const cell = cells[row * width + col];
+      const cellEl = document.createElement("div");
+      cellEl.classList.add("grid-cell");
+      if (cell.clues?.length) {
+        cellEl.dataset.clues = JSON.stringify(cell.clues);
+      }
+      if (cell.label) {
+        const labelEl = document.createElement("span");
+        labelEl.classList.add("label");
+        labelEl.textContent = cell.label;
+        cellEl.appendChild(labelEl);
+      }
+      if (!cell.answer) {
+        cellEl.classList.add("black");
+      }
+      if (cell.answer) {
+        const answerEl = document.createElement("input");
+        answerEl.classList.add("answer");
+        // answerElement.value = cell.answer;
+        cellEl.appendChild(answerEl);
+      }
+      gridEl.appendChild(cellEl);
+    }
+  }
+}
+
+function loadNYTXW(json) {
+  loadMetadata(json);
+  buildClueLists(json.body[0].clueLists, json.body[0].clues);
+  buildGrid(json.body[0].dimensions, json.body[0].cells);
+  puzzleContainerEl.classList.remove("hidden");
+}
